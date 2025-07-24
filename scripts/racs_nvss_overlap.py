2888dab48938e323d1e64e230d207288a1afd23b
Oliver Oayda <oaydaoliver@gmail.com>
2025-07-24 11:35:30 +1000

    Add scripts for masking and cross-matching RACS & NVSS

--- scripts/racs_nvss_overlap.py
+++ scripts/racs_nvss_overlap.py
@@ -1,7 +1,7 @@
 # %%
 from dipoleutils.utils import DataLoader, CrossMatch, CatalogueToMap
 import matplotlib.pyplot as plt
-from astropy.table import Table
+from astropy.table import Table, join
 import healpy as hp
 import numpy as np
 # %%
@@ -20,6 +20,7 @@ nvss.add_coordinate_system(starting_frame='equatorial', target_frame='galactic')
 
 racs.make_cut('total_flux_source', 15, 1000)
 nvss.make_cut('integrated_flux', 15, 1000)
+# %%
 # mask a bit extra since max racs dex is 29.127 in flux range
 MIN_DEC = -40; MAX_DEC = 29
 GMASK_B = 5
@@ -54,3 +55,20 @@ print(
     f'% of RACS sources with counterpart NVSS: '
     f'{(xmatch.get_number_of_matches() / racs.get_source_count())*100:.2f}'
 )
+# %%
+# five brightest RACS sources with no NVSS match
+tab = xmatch.get_crossmatch_table()
+no_match = tab['source_idx_B'] == -1
+no_match_tab = tab[no_match]
+no_match_tab.rename_column('source_name_A', 'source_name')
+
+no_match_full = join(
+    no_match_tab,
+    racs.get_catalogue(),
+    keys='source_name',
+    join_type='left'
+)
+no_match_full.sort('total_flux_source', reverse=True)
+print(no_match_full[:5]['source_name', 'total_flux_source'])
+
+# %%
