c0d86abc829c4e2db86bf8cce961d1dfa80ce132
Oliver Oayda <oliver.oayda@sydney.edu.au>
2026-09-11 15:16:07 +0200

    add mid1 with corrected declination column

--- dipoleutils/utils/file_names.py
+++ dipoleutils/utils/file_names.py
@@ -34,6 +34,7 @@ fn_dict = {
         'low3': 'RACS-low3_sources.fits',
         'low3-scaled': 'RACS-low3_sources_scaled.fits',
         'mid1': 'RACS-mid1_noscale_srl.fits',
+        'mid1-withcor': 'RACS-mid1_noscale_srl-withcor.fits',
         'mid1-scaled': 'AS110_Derived_Catalogue_racs_mid_sources_v01_15372.csv',
         'mid1-25as': 'RACS-mid1_25arcsec_sources.csv',
         'high1': 'RACS-high_sources_21-01-25.fits'
